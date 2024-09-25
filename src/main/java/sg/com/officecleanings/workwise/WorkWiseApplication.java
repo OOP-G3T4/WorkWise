6e6ab608b3870bc7c8049a73de4f377d150f2ba5
poojunsui <My name>
2024-09-26 01:49:46 +0800

    remove files, add dependencies and config

--- src/main/java/sg/com/officecleanings/workwise/WorkWiseApplication.java
+++ src/main/java/sg/com/officecleanings/workwise/WorkWiseApplication.java
@@ -2,8 +2,10 @@ package sg.com.officecleanings.workwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class WorkWiseApplication {
 
     public static void main(String[] args) {
