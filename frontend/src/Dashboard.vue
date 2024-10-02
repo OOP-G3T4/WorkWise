55e5a6f0a2d5e7ece5bda41d7d0b677536fa5fcc
tan-sd <shengdatan@gmail.com>
2024-10-03 00:20:01 +0800

    Initialise Dashboard view

---
+++ frontend/src/Dashboard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import JobDetails from "./components/JobDetails.vue";
+</script>
+
+<template>
+    <div class="d-flex">
+        <JobDetails v-for="(item, value) in testarr" :key="value" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            testarr: [1, 2, 3, 4, 5],
+        };
+    },
+};
+</script>
