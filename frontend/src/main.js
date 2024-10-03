bb06d36c6f937d9c2cd19beafd3c91f50df14572
tan-sd <shengdatan@gmail.com>
2024-10-03 20:07:17 +0800

    Import FontAwesome and used it in Navbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+import { faUser, faCalendar, faUmbrella, faGear, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser, faCalendar, faUmbrella, faGear, faRightFromBracket);
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
