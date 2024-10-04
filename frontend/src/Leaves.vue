51c42cc94161bb911915673771df957991880b50
tan-sd <shengdatan@gmail.com>
2024-10-05 00:26:16 +0800

    Initialise Leaves page

---
+++ frontend/src/Leaves.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+    <div class="d-flex">
+        <Navbar />
+        <div class="">This is the leaves page</div>
+    </div>
+</template>
