33255b01c553b70753930fa3dfd0d7b78f288c70
tan-sd <shengdatan@gmail.com>
2024-10-05 00:26:43 +0800

    Import DatePicker

--- frontend/src/Dashboard.vue
+++ frontend/src/Dashboard.vue
@@ -1,13 +1,17 @@
 <script setup>
 import JobDetails from "./components/JobDetails.vue";
 import Navbar from "./components/Navbar.vue";
+import DatePicker from "./components/DatePicker.vue";
 </script>
 
 <template>
     <div class="d-flex">
         <Navbar />
-        <div class="d-flex">
-            <JobDetails v-for="(item, value) in testarr" :key="value" />
+        <div class="calendar-container d-flex flex-column">
+            <DatePicker />
+            <div class="d-flex">
+                <JobDetails v-for="(item, value) in testarr" :key="value" />
+            </div>
         </div>
     </div>
 </template>
