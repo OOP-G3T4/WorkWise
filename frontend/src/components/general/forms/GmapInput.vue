e825dc187005fb630193a02833f88bb946ce7447
Adam Bin Firdaus Tan <adambft98@gmail.com>
2024-10-27 11:13:04 +0800

    added gmaps and dropdown templates components

---
+++ frontend/src/components/general/forms/GmapInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <input v-model="value" />
+</template>
+
+<script>
+export default {
+    props: {
+        inputValue: {
+            type: String,
+            required: false,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            value: this.inputValue,
+        }
+    },
+    watch: {
+        value() {
+            // Listens for changes in this.value
+            this.emitValue();
+        }
+    },
+    methods: {
+        emitValue() {
+            // Sends the value to the parent component
+            this.$emit('input', this.value);
+        }
+    },
+}
+</script>
