c5fb0827df53cf00d199f0c30ed55c420c623479
Glen Low <glen.low.2021@scis.smu.edu.sg>
2024-11-17 21:30:59 +0800

    change date to today (softcode)

--- backend/src/main/java/sg/com/officecleanings/workwise/service/JobService.java
+++ backend/src/main/java/sg/com/officecleanings/workwise/service/JobService.java
@@ -95,7 +95,7 @@ public class JobService {
 
     public boolean createJobsFromActiveSubscriptions() {
         System.out.println("Creating jobs from active subscriptions method called.");
-        LocalDate today = LocalDate.of(2024, 11, 17);
+        LocalDate today = LocalDate.now();
 
         LocalDate targetWeekStart = today.plusWeeks(4).with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY)).plusDays(1);
         LocalDate targetWeekEnd = targetWeekStart.plusDays(6);
